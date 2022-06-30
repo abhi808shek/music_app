0c5049787da08b96767d155d86415659907fba7c
Sachitmaster <sachitkumarsahu6@gmail.com>
2022-06-30 13:37:37 +0530

    searchpage

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import Navbar from './Components/Navbar/Navbar';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Loginpage from './Components/Loginpage/Loginpage'
 import Homepage from './Components/Homepage/Homepage';
-import Searchpage from './Components/Searchpage/Searchpage';
+import Searchpage from './Components/Searchpage/searchpage';
 
 function App() {
   return (
-  <>
+    <>
       <Navbar />
-    <Sidebar /> 
-    <Footer />
-    <Loginpage />
-    <Homepage />
-    <Searchpage />
-</>
-   
-    
+      <Sidebar />
+      <Footer />
+      <Loginpage />
+      <Homepage />
+      <Searchpage />
+    </>
+
+
   );
 }
 
--- src/Components/Searchpage/Searchpage.jsx
+++ src/Components/Searchpage/Searchpage.jsx
@@ -5,86 +5,105 @@ import searchwhite1 from "./searchwhite1.png"
 import musicalbum1 from "./musicalbum1.png"
 import Vector2 from "./Vector2.png"
 import image17 from "./image17.png"
+import search from "./search.css"
 
-function Searchpage() {
+function searchpage() {
   return (
     <>
-     <div className="search_container">
+            <div className="search_container">
         <div className="search_container1">
-            <img src={Ellipse39} alt="" className="search_logo"/>
-            <div className="search_navbar">
-                <div className="search_nav">
-                    <img src={Vector} alt="" className="search_icon"/>
-                    <p className="search_text">Home</p>
-                </div>
-                <div className="search_nav">
-                    <img src={searchwhite1} alt="" className="search_icon"/>
-                    <p className="search_text">Search</p>
-                </div>
-                <div className="search_nav">
-                    <img src={musicalbum1} alt="" className="search_icon"/>
-                    <p className="search_text">Album</p>
-                </div>
-                <div className="search_nav">
-                    <img src={Vector2} alt="" className="search_icon"/>
-                    <p className="search_text">Liked songs</p>
-                </div>
-            </div>
+            <h1 className="head1">Username</h1>
+            <h1 className="head1">Logout</h1>
         </div>
         <div className="search_container2">
             <div className="search_container3">
-                <button className="search_btn1">Username</button>
-                <button className="search_btn1">Logout</button>
+                <img src={Ellipse39} alt="logo" className="logo"/>
+                <div className="navbar">
+                    <div className="nav">
+                        <div className="icon">
+                            <img src={Vector} alt="" className="icon1"/>
+                        </div>
+                        <div className="text">
+                            <h1 className="head2">Home</h1>
+                        </div>
+                    </div>
+                    <div className="nav">
+                        <div className="icon">
+                            <img src={searchwhite1} alt="" className="icon1"/>
+                        </div>
+                        <div className="text">
+                            <h1 className="head2">Search</h1>
+                        </div>
+                    </div>
+                    <div className="nav">
+                        <div className="icon">
+                            <img src={musicalbum1} alt="" className="icon1"/>
+                        </div>
+                        <div className="text">
+                            <h1 className="head2">Album</h1>
+                        </div>
+                    </div>
+                    <div className="nav">
+                        <div className="icon">
+                            <img src={Vector2} alt="" className="icon1"/>
+                        </div>
+                        <div className="text">
+                            <h1 className="head2">Liked songs</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="search_container4">
                 <div className="search_container5">
-                    <h1 className="search_heading">Search</h1>
+                    <h1 className="head3">Search</h1>
                     <div className="search_container6">
-                        <input type="text" className="search_inputbox" placeholder="Artists, Songs"/>
-                        <img src={image17} alt="" className="search_audio"/>
+                        <input type="text" placeholder="Artists, Songs" className="input"/>
+                        <img src={image17} alt="" className="icon2"/>
                     </div>
-                    <hr/>
                 </div>
+                <hr/>
                 <div className="search_container7">
-                    <h1 className="search_heading2">Genre</h1>
-                    <p className="search_text2">See More</p>
-                </div>
-                <div className="search_cards">
-                    <div className="search_card search_card1">
-                        <h1 className="search_title2">Disco</h1>
+                    <div className="search_container8">
+                        <h1 className="head4">Genre</h1>
+                        <h1 className="head5">See more</h1>
                     </div>
-                    <div className="search_card search_card2">
-                        <h1 className="search_title2">House Music</h1>
+                    <div className="search_container9">
+                        <div className="box box1">
+                            <h1 className="head6">Dicso</h1>
+                        </div>
+                        <div className="box box2">
+                            <h1 className="head6">House Music</h1>
+                        </div>
+                        <div className="box box3">
+                            <h1 className="head6">Rapping</h1>
+                        </div>
+                        <div className="box box4">
+                            <h1 className="head6">Trance</h1>
+                        </div>
+                        <div className="box box5">
+                            <h1 className="head6">Pop Rock</h1>
+                        </div>
                     </div>
-                    <div className="search_card search_card3">
-                        <h1 className="search_title2">Rapping</h1>
+                    <div className="search_container8">
+                        <h1 className="head4">Artists</h1>
+                        <h1 className="head5">See more</h1>
                     </div>
-                    <div className="search_card search_card4">
-                        <h1 className="search_title2">Trance</h1>
-                    </div>
-                    <div className="search_card search_card5">
-                        <h1 className="search_title2">Pop Rock</h1>
-                    </div>
-                </div>
-                <div className="search_container7">
-                    <h1 className="search_heading2">Artists</h1>
-                    <p className="search_text2">See More</p>
-                </div>
-                <div className="search_cards">
-                    <div className="search_card search_card6">
-                        <h1 className="search_title2">Disco</h1>
-                    </div>
-                    <div className="search_card search_card7">
-                        <h1 className="search_title2">House Music</h1>
-                    </div>
-                    <div className="search_card search_card8">
-                        <h1 className="search_title2">Rapping</h1>
-                    </div>
-                    <div className="search_card search_card9">
-                        <h1 className="search_title2">Trance</h1>
-                    </div>
-                    <div className="search_card search_card10">
-                        <h1 className="search_title2">Pop Rock</h1>
+                    <div className="search_container9">
+                        <div className="box box6">
+                            <h1 className="head6">Ed Sheeran</h1>
+                        </div>
+                        <div className="box box7">
+                            <h1 className="head6">Taylor Swift</h1>
+                        </div>
+                        <div className="box box8">
+                            <h1 className="head6">Adele</h1>
+                        </div>
+                        <div className="box box9">
+                            <h1 className="head6">Eminem</h1>
+                        </div>
+                        <div className="box box10">
+                            <h1 className="head6">Bruno Mars</h1>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -94,4 +113,4 @@ function Searchpage() {
   )
 }
 
-export default Searchpage
+export default searchpage
