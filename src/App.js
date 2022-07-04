6e018586478698362490824bb080f33b21219893
Abhishek gupta <abhi808gupta@gmail.com>
2022-07-04 23:29:56 +0530

    basic changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
   <>
       <Navbar />
-    <Sidebar /> 
-    <Footer />
+    {/* <Sidebar /> 
+    <Footer /> */}
 </>
    
     
---
+++ src/sdbjd.jsx
@@ -0,0 +1,8 @@
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+<div className="container">
+    <div className="leftSidebar">
+        <Sidebar />
+    </div>
+    {/* <div className="contentBox"></div> */}
+</div>
