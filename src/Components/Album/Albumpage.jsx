8b2dc6652887f50c61a35f802aca89ef3986c67a
Abhishek gupta <abhi808gupta@gmail.com>
2022-07-08 12:55:21 +0530

    Mapping Cards

--- src/Components/Album/Albumpage.jsx
+++ src/Components/Album/Albumpage.jsx
@@ -4,7 +4,7 @@ import './Albumstyle.css'
 import Rectangle74 from './Rectangle74.png'
 
 const Album = () => {
-  const items = [1,2,3,4,5,6,7]
+  const items = [1,2,3,4,5,6,7,8]
   return (
     <>
       <div className="album-container">
---
+++ src/Components/Likedpage/LikedPageCard/LikedPageCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./likedPageCard.css"
+
+
+const LikedPageCard = ({Rectangle62,Vector}) => {
+  return (
+    <>
+        
+          <div className="liked-img-container1">
+            <img src={Rectangle62} alt="" className="liked-icon" />
+          </div>
+          <div className="liked-txt">
+            <p className="liked-para1">I  Wanna Be Yours</p>
+            <p className="liked-para2">Arctic Monkeys</p>
+          </div>
+          <div className="liked-like">
+            <img src={Vector} alt="" className="liked-like-icon" />
+          </div>
+        
+    </>
+  )
+}
+
+export default LikedPageCard;
