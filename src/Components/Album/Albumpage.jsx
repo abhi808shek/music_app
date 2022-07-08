96f24ef16139cf1d23c26c8c62b9ca201af49317
Sachitmaster <sachitkumarsahu6@gmail.com>
2022-07-08 15:35:10 +0530

    update

--- src/Components/Album/Albumpage.jsx
+++ src/Components/Album/Albumpage.jsx
@@ -4,11 +4,7 @@ import './Albumstyle.css'
 import Rectangle74 from './Rectangle74.png'
 
 const Album = () => {
-<<<<<<< HEAD
   const items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
-=======
-  const items = [1,2,3,4,5,6,7,8]
->>>>>>> 8b2dc6652887f50c61a35f802aca89ef3986c67a
   return (
     <>
       <div className="album-container">
